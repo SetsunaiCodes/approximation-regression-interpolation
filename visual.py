e63e16e9b5f0cff413d883238dcddaf45005a69f
SetsunaiCodes <nicojin.m0sic@hotmail.com>
2024-03-24 14:45:42 +0100

    initial

--- visual.py
+++ visual.py
@@ -1,4 +1,4 @@
-#Hier wird basierend auf equation.py die Visualisierung generiert
+# Hier wird basierend auf equation.py die Visualisierung generiert
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -8,7 +8,7 @@ N = 21
 # 11 Werte zwischen 0 und 10 für die X Achse definieren
 x = np.linspace(0, 10, 11)
 # 11 willkürliche Werte für die Y Achse definieren
-y = [3.9, 4.4, 10.8, 10.3, 11.2, 13.1, 14.1,  9.9, 13.9, 15.1, 12.5]
+y = [3.9, 4.4, 10.8, 10.3, 11.2, 13.1, 14.1, 9.9, 13.9, 15.1, 12.5]
 
 ######### DISPLAY AREA ###########
 
@@ -16,13 +16,14 @@ y = [3.9, 4.4, 10.8, 10.3, 11.2, 13.1, 14.1,  9.9, 13.9, 15.1, 12.5]
 # fit a linear curve and estimate its y-values and their error.
 a, b = np.polyfit(x, y, deg=1)
 y_est = a * x + b
-y_err = x.std() * np.sqrt(1/len(x) +
-                          (x - x.mean())**2 / np.sum((x - x.mean())**2))
+y_err = x.std() * np.sqrt(
+    1 / len(x) + (x - x.mean()) ** 2 / np.sum((x - x.mean()) ** 2)
+)
 
 fig, ax = plt.subplots()
-ax.plot(x, y_est, '-')
+ax.plot(x, y_est, "-")
 ax.fill_between(x, y_est - y_err, y_est + y_err, alpha=0.2)
-ax.plot(x, y, 'o', color='tab:brown')
+ax.plot(x, y, "o", color="tab:brown")
 
-#Return der Visualisierung
+# Return der Visualisierung
 plt
