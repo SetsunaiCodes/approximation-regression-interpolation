2493864eb25ed3269b9f64d3d4ad4fb611b9fd46
SetsunaiCodes <nicojin.m0sic@hotmail.com>
2024-04-04 15:12:33 +0200

    Finished skeleton

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-# | Import und Initialisierung |
+# Import und Initialisierung
+
+# Rechnungsimports
 import pandas as pd
 import plotly.express as px
+
+# Visualimports
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
@@ -9,34 +13,74 @@ import statsmodels.api as sm
 # Lese die Daten ein (temporäre Mock-Excel-Tabelle)
 df = pd.read_csv("MockBook.csv", delimiter=";")
 
-
-# | Rechnungen über build in Rechenoperationen |
+# Rechnungen über build in Rechenoperationen
 X = df["X"]  # Unabhängige Variable
 X = sm.add_constant(X)  # Konstante hinzufügen
 Y = df["Y"]  # Abhängige Variable
 model = sm.OLS(Y, X).fit()  # Lineare Regression
-# Vorhersagen berrechnen
+# Vorhersagen errechnen
 predictions = model.predict(X)
 
-# | App-Layout erstellen |
+# App-Layout erstellen
 app = dash.Dash(__name__)
+
+# Setze die externe CSS-Datei mit benutzerdefinierten Stilregeln
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+
+# Initialisiere die Dash-App mit externen Stilen
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(
     [
-        html.H1("Linear Regression Plot", style={"text-align": "center"}),
-        dcc.Graph(id="linear-regression-plot"),
-    ]
+        html.Div(
+            style={"height": "18vh", "background-color": "red"},
+            children=[
+                dcc.Input(
+                    id="input_1",
+                    type="text",
+                    placeholder="input type 1",
+                )
+            ],
+        ),
+        html.Div(
+            style={"display": "flex"},
+            children=[
+                html.Div(
+                    style={
+                        "width": "80vw",
+                        "height": "75vh",
+                        "background-color": "blue",
+                    },
+                    children=[
+                        dcc.Graph(id="linear-regression-plot"),
+                    ],
+                ),
+                html.Div(
+                    style={
+                        "width": "20vw",
+                        "height": "75vh",
+                        "background-color": "green",
+                    },
+                    children=[
+                        html.P("Hier stehen Analytics", style={"text-align": "center"})
+                    ],
+                ),
+            ],
+        ),
+    ],
 )
 
-# | Callback erstellen, damit der Plot aktualisiert werden kann |
+
+# Callback erstellen, damit der Plot aktualisiert werden kann
 @app.callback(
     Output("linear-regression-plot", "figure"),
-    [Input("linear-regression-plot", "id")],
+    [Input("input_1", "value")],
 )
-def update_plot(id):
+def update_plot(input_value):
     # Generieren des Plots
     fig = px.scatter(df, x="X", y="Y", trendline="ols")
     return fig
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
